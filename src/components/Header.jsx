ff88f801d01aa2c9a56e0495838d90036ad4a5e1
danyal <danyallsty04@gmail.com>
2023-08-23 05:55:49 -0700

    header component created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({title}) => {
   return (
-    <div>Header</div>
+    <div className='my-4'>
+      <p className='capitalize text-gray-400 font-bold'>page</p>
+      <h1 className='capitalize text-2xl font-bold'>{title}</h1>
+    </div>
   )
 }
 
