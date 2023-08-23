32b1a68c68e02d50814aff2650217cdfc109fe27
danyal <danyallsty04@gmail.com>
2023-08-23 05:57:33 -0700

    employees page created

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { Header } from '../components';
+import { GridComponent,Search,ColumnsDirective, ColumnDirective, ContextMenu, Page, Inject ,Toolbar } from '@syncfusion/ej2-react-grids';
+
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
+import { employeesData,employeesGrid } from '../data/dummy';
 
 const Employees = () => {
+  const editing = { allowDeleting: true, allowEditing: true };
   return (
-    <div>Employees</div>
+    <div className='bg-white m-2 md:m-10 p-2 md:p-10 rounded-lg'>
+      <Header title='Employees'/>
+
+      <GridComponent
+        id="gridcomp"
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        allowExcelExport
+        allowPdfExport
+        contextMenuItems={contextMenuItems}
+        editSettings={editing}
+        toolbar={['Search']}
+        width='auto'
+       
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {employeesGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+        <Inject services={[Page,Search,Toolbar]} />
+      </GridComponent>
+    </div>
   )
 }
 
-export default Employees
+export default   Employees
