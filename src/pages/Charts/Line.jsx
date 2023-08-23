4f6f88943be8d87681c4045f1df9f12b3f99eae8
danyal <danyallsty04@gmail.com>
2023-08-23 07:32:13 -0700

    line page created

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,29 +1,14 @@
 import React from 'react';
-import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, LineSeries, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts';
 
-import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy';
-import { useStateContext } from '../../contexts/ContextProvider';
+import { ChartsHeader, LineChart } from '../../components';
 
-const Line = () => {
-  return (
-    <div className='m-7 p-5 rounded-lg bg-white'>
-    <ChartComponent
-    id="line-chart"
-    height="420px"
-    primaryXAxis={LinePrimaryXAxis}
-    primaryYAxis={LinePrimaryYAxis}
-    chartArea={{ border: { width: 0 } }}
-    tooltip={{ enable: true }}
-   
-    legendSettings={{ background: 'white' }}
-  >
-    <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
-    <SeriesCollectionDirective>
-      {lineCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
-    </SeriesCollectionDirective>
-  </ChartComponent>
+const Line = () => (
+  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+    <ChartsHeader category="Line" title="Inflation Rate" />
+    <div className="w-full">
+      <LineChart />
+    </div>
   </div>
-  )
-}
+);
 
-export default Line
+export default Line;
